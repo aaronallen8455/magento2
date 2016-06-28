5dd193bb30c0482aa8e70b7eeabc8c9e937260da
Aaron Allen <aaronallen8455@gmail.com>
2016-06-28 04:48:00 -0500

    6/28/16

--- app/code/AAllen/KnockOut/view/frontend/web/js/koexample.js
+++ app/code/AAllen/KnockOut/view/frontend/web/js/koexample.js
@@ -8,6 +8,25 @@ define(['uiComponent'],
             initialize: function () {
                 this._super();
                 this.sayHello = 'Hello this is content populated with KO!';
+                this.time = new Date().toTimeString();
+                this.color = 'red';
+                //time is defined as observable
+                this.observe(['time', 'color']);
+                //update every second
+                window.setInterval(this.flush.bind(this), 1000);
+            },
+            flush: function () {
+                var time = new Date();
+                this.time(time.toTimeString());
+                this.color(this.randomHex());
+            },
+            randomHex: function () {
+                var chars = '0123456789ABCDEF';
+                var result = '#';
+                for (var i=0; i<6; i++) {
+                    result += chars[Math.floor(Math.random()*12)];
+                }
+                return result;
             }
         });
     }
