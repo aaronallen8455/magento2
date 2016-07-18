d599f7d5641b3d680423f12bdf1c86dc957155ab
Aaron Allen <aaronallen8455@gmail.com>
2016-07-18 05:39:58 -0500

    extending uiClass

---
+++ app/code/AAllen/CustomerAtt/Setup/InstallData.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aaron Allen
+ * Date: 7/18/2016
+ * Time: 2:35 AM
+ */
+
+namespace AAllen\CustomerAtt\Setup;
+
+
+class InstallSchema extends 
+{
+
+}
